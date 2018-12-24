7b9b2e3bc5ead46b928e434aebc90df7cf6587da
Muhammad Arsalan Ahmed <cs141012@dsu.edu.pk>
2018-12-24 15:19:42 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@
 module.exports = {
     mongodb: {
         //mongodb connection string goes here
-        dbURI: 'mongodb://arsalan:arsalan123@ds127604.mlab.com:27604/nodeapp'
+        dbURI: 'Connection key to mongodb goes here';
     }
 };
